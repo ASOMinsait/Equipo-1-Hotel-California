408c565ca43ff5d8244b3044de1f3d3a83db3b35
Isaul Mendieta Galindo <imendieta@minsait.com>
2023-03-03 13:14:33 -0600

    añadiendo habitaciones

---
+++ habitaciones/target/classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO habitaciones (id_habitacion,maximo_personas,precio_noche,estatus) VALUES (1,3,1750,1);
+INSERT INTO habitaciones (id_habitacion,maximo_personas,precio_noche,estatus) VALUES (2,2,1350,1);
+INSERT INTO habitaciones (id_habitacion,maximo_personas,precio_noche,estatus) VALUES (3,6,3350,0);
+INSERT INTO habitaciones (id_habitacion,maximo_personas,precio_noche,estatus) VALUES (4,5,3150,0);
